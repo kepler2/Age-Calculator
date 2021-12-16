8ecdd11477ecdd3dfcbaf3e3a4134bea865326b1
Kepler <dubfire08@gmail.com>
2021-12-16 16:08:12 +0200

    update

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ const year = document.querySelector('#futureYear')
 const goBtn = document.querySelector('#goBtn')
 const resetBtn = document.querySelector('#resetBtn')
 const resultTxt = document.querySelector('#resultTxt')
+const yearBornTxt = document.querySelector('#yearBorn')
+const year100Txt = document.querySelector('#year100')
+const retirementTxt = document.querySelector('#retirement')
 
 window.addEventListener('load', function(){
     age.focus()
@@ -24,7 +27,10 @@ function clearFields(){
     age.value = ''
     year.value = ''
     age.focus()
-    resultTxt.textContent = 'Result will appear here.'
+    resultTxt.innerHTML = ''
+    yearBornTxt.innerHTML = ''
+    year100Txt.innerHTML = ''
+    retirementTxt.innerHTML= ''
 }
 
 function calculateYear(){
@@ -35,7 +41,13 @@ function calculateYear(){
     }
     else {
         result = parseInt((futureYear - 2021) + currentAge) 
+        birthDate = parseInt(2021 - currentAge)
+        yearOneHundred = parseInt(100 - currentAge) + 2021
+        retirementAge = parseInt(65 - currentAge) + 2021
         resultTxt.innerHTML = `You will be ${result} years old in ${futureYear}!`
+        yearBornTxt.innerHTML = `You were born in ${birthDate}.`
+        year100Txt.innerHTML = `You will be 100 years old in ${yearOneHundred}.`
+        retirementTxt.innerHTML = `You will retire in ${retirementAge}.`
     }
 }      
 
